10b7dea0825a0b0e90957c755c1ad65d709b9082
edureka <edureka@gmail.com>
2024-01-10 12:43:49 +0530

    commited the app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Welcome To Edureka! Learning Center Cloud&DevOps - Mini-Project CodePipeline Using AWS'));
+app.get('/', (req, res) => res.send('Hello Welcome To Edureka! Mini-Project'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
