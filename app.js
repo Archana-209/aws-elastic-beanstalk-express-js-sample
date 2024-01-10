6dddd2947e5e4a50e6f0bd134dd7abd5fa05a2d7
edureka <edureka@gmail.com>
2024-01-10 12:28:47 +0530

    Modified the code source

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Welcome To Edureka! Learning Center
-Cloud&DEvOps - Mini-Project 
-CodePipeline Using AWS'));
+app.get('/', (req, res) => res.send('Hello Welcome To Edureka! Learning Center Cloud&DevOps - Mini-Project CodePipeline Using AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
